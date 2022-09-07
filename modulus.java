b5f8b2a0e661898a323a27dc4cb6ec9cbe941dbd
AyushTiwari <ayush@wowfinstack.com>
2022-09-07 17:11:29 +0530

    File Commit

---
+++ modulus.java
@@ -0,0 +1,8 @@
+public class modulus {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        System.out.println("The modulus of " + a + " & " + b + " is " + (a % b));
+    }
+}
